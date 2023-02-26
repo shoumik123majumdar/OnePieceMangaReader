7da2ef92bf8232959d34889a43ab69482c97dee8
Shoumik Majumdar <shoumik123majumdar@gmail.com>
2023-02-26 15:40:31 -0800

    Integrated the dataframe into the main.py so that the program runs 'currentChapter' based off of the schedule in the dataframe. Also fixed up some bugs in my error handling section of the program.

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
 import webbrowser
 import schedule
+import pandas as pd
 
 url = 'https://opscans.com/manga/72/vol-tbe-chapter-'
 chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
 
+database = pd.read_csv('ReleaseDates.csv', index_col=None)
+
+global currentChapterIndex
+with open('index.txt', 'r') as f:
+    currentChapterIndex = int(f.readlines()[-1])
+
+
+def addIndex():
+    global currentChapterIndex
+    currentChapterIndex += 1
+    with open('index.txt', 'a') as f:
+        f.write("\n" + str(currentChapterIndex))
+
+
+schedule.every().friday.do(addIndex)
+
 global currentChapter
-currentChapter= 1076
-
-
-def addChapterNumber():
-    global currentChapter
-    currentChapter+=1
-
-
-schedule.every().friday.do(addChapterNumber)
+currentChapter = database._get_value(currentChapterIndex, 'Chapters')
 
 isInput = True
 prompt = "Enter one piece chapter (1000 and up) or enter 'current' to go to said chapter:"
-while(isInput):
+while (isInput):
     chapterNumber = input(prompt)
-    if(chapterNumber == "current"):
+    if (chapterNumber == "schedule"):
+        print(database)
+    elif (chapterNumber == "current"):
         isInput = False
-    elif (float(chapterNumber).is_integer() and int(chapterNumber) > 1000):
-        isInput = False
+    elif (chapterNumber.isnumeric()):
+        if (float(chapterNumber).is_integer() and int(chapterNumber) > 1000):
+            isInput = False
     else:
-        prompt = "This is not an acceptable input, please try again:"
+        if (chapterNumber != "schedule"):
+            prompt = "This is not an acceptable input, please try again:"
+        else:
+            prompt = "Enter one piece chapter (1000 and up) or enter 'current' to go to said chapter:"
 
-
-if(chapterNumber == "current"):
-    url += str(currentChapter)+"/"
-elif int(chapterNumber) < currentChapter-3:
-    url = 'https://opscans.com/manga/72/vol-tbe-ch-'+str(chapterNumber)+"/"
+if (chapterNumber == "current"):
+    if currentChapter == "WSJ Break" or currentChapter == "Oda Break":
+        print("Sorry, One Piece is on break this week, try again next week")
+        exit(code=0)
+    url += str(currentChapter) + "/"
+elif int(chapterNumber) < int(currentChapter) - 3:
+    url = 'https://opscans.com/manga/72/vol-tbe-ch-' + str(chapterNumber) + "/"
 else:
     url += str(chapterNumber) + "/"
 
--- scraper.py
+++ scraper.py
@@ -7,29 +7,27 @@ driver = webdriver.Chrome()
 driver.get("https://claystage.com/one-piece-chapter-release-schedule-for-2023")
 time.sleep(2)
 
-weeksList = []
 chapterList = []
 dateList = []
 for i in range(1,53):
-    xPath = '//*[@id="post-3990"]/div/div[2]/figure/table/tbody/tr[' + str(i) + ']/td'
-    weeksList.append(driver.find_element(By.XPATH,xPath).text)
     xPath = '//*[@id="post-3990"]/div/div[2]/figure/table/tbody/tr[' + str(i) + ']/td[2]'
     chapterList.append(driver.find_element(By.XPATH,xPath).text)
     xPath = '//*[@id="post-3990"]/div/div[2]/figure/table/tbody/tr[' + str(i) + ']/td[3]'
     dateList.append(driver.find_element(By.XPATH,xPath).text)
 
-df = pd.DataFrame(zip(weeksList,chapterList,dateList),columns=['Weeks','Chapters',"Release Dates"])
+df = pd.DataFrame(zip(chapterList,dateList),columns=['Chapters',"Release Dates"])
+df.to_csv("ReleaseDates.csv")
 print(df)
 
 currentChapter = df._get_value(8,'Chapters')
 print(currentChapter)
 
 
-
 '''
 XPATH HOLDER
 //*[@id="post-3990"]/div/div[2]/figure/table/tbody/tr[2]/td[2]
 //*[@id="post-3990"]/div/div[2]/figure/table/tbody/tr[2]/td[2]
-
+ xPath = '//*[@id="post-3990"]/div/div[2]/figure/table/tbody/tr[' + str(i) + ']/td'
+    weeksList.append(driver.find_element(By.XPATH,xPath).text)
 weeks = driver.find_element(By.XPATH,'//*[@id="post-3990"]/div/div[2]/figure/table/tbody/tr')
 '''
