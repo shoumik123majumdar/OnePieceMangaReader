1438d788d190112926d38a69ed545806f093b073
Shoumik Majumdar <shoumik123majumdar@gmail.com>
2023-03-05 02:27:16 -0500

    Added a check to see if when the next schedule addIndex is. Bugfix again with some functionalities that were down during a perceived break week.

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ with open('index.txt', 'r') as f:
     currentChapterIndex = int(f.readlines()[-1])
 
 
+
 def addIndex():
     global currentChapterIndex
     currentChapterIndex += 1
     with open('index.txt', 'a') as f:
-        f.write(str(currentChapterIndex))
+        f.write(str(currentChapterIndex)+"\n")
 
 
-schedule.every().friday.do(addIndex)
+schedule.every().sunday.do(addIndex)
 
+global comparableChapter
 global currentChapter
 currentChapter = database._get_value(currentChapterIndex, 'Chapters')
+comparableChapter = currentChapter
+if currentChapter == "WSJ Break" or currentChapter == "Oda Break":
+    comparableChapter = database._get_value(currentChapterIndex - 1, 'Chapters')
+
 
 isInput = True
 prompt = "Enter one piece chapter (1000 and up) or enter 'current' to go to said chapter:"
@@ -33,7 +39,7 @@ while (isInput):
     elif (chapterNumber == "current"):
         isInput = False
     elif (chapterNumber.isnumeric()):
-        if (float(chapterNumber).is_integer() and int(chapterNumber) > 1000 and int(chapterNumber)<int(currentChapter)):
+        if (float(chapterNumber).is_integer() and int(chapterNumber) > 1000 and int(chapterNumber)<=int(comparableChapter)):
             isInput = False
         else:
             prompt = "Website doesn't support the chapter you selected, enter again:"
@@ -47,11 +53,13 @@ while (isInput):
 if (chapterNumber == "current"):
     if currentChapter == "WSJ Break" or currentChapter == "Oda Break":
         print("Sorry, One Piece is on break this week, try again next week")
+        print("Update is set to run next on " + str(schedule.next_run()))
         exit(code=0)
     url += str(currentChapter) + "/"
-elif int(chapterNumber) < int(currentChapter) - 3:
+elif int(chapterNumber) < int(comparableChapter) - 3:
     url = 'https://opscans.com/manga/72/vol-tbe-ch-' + str(chapterNumber) + "/"
 else:
     url += str(chapterNumber) + "/"
 
 webbrowser.get(chrome_path).open(url)
+print("Update is set to run next on " + str(schedule.next_run()))
